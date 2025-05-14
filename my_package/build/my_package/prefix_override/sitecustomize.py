cfab53571b6c78716a080a559a51ffd80d698a0f
Yui-Maruyama <u451328k@ecs.osaka-u.ac.jp>
2025-05-14 21:49:40 +0900

    ros launchコマンドで動作するプログラムを作れました，まだ完全に並列になっていない気がするのでここから修正します.あと一部ファイル消しました

--- my_package/build/my_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/c/research/program/ADMM_with_ICNN_on_ROS/my_package/install/my_package'
--- my_package/build/my_package/share/my_package/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/mnt/c/research/program/ADMM_with_ICNN_on_ROS/my_package/build/my_package"
